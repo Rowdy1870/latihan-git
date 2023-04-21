a2e99013a74285c50b76a8bc524c6304f803de99
Rowdy1870 <93893800+Rowdy1870@users.noreply.github.com>
2023-04-21 07:13:24 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Anggoro</li>
+        <li>Nama Belakang : Kriswanto</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Rasamala 4, Gg.H.Rosyid, No.32i</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Yogyakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Sepak Bola</li>
+    </ul>
+</section>
